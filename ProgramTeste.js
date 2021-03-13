486e2e7e9ee5e9708f55220f789c7a0cbb4aa4e1
karenmentges <karen_ruvermentges@hotmail.com>
2021-03-12 21:59:25 -0300

    Changes

--- ProgramTeste.js
+++ ProgramTeste.js
@@ -3,10 +3,34 @@ var scene;      //Mundo Virual
 var camera;     //Área de Visualização
 var renderer;   //Responsável por renderizar
 
+var elementos = [];
+
+var velocidade = 0.5;
+
 var criaCubo = function(){
     let geometria = new THREE.BoxGeometry(10, 10, 10);
-    
+
+    let pink   = new THREE.Color("rgb(200,33,81)");
+    let green  = new THREE.Color("rgb(64,224,208)");
+    //let purple = new THREE.Color(148,0,211);
+    //let colors = [pink, green, purple];
+
+
+    let material = new THREE.MeshBasicMaterial({color: pink});
+    let cubo = new THREE.Mesh(geometria, material); //Mesh(malha), junta a geometria e o material, criando uma malha de triângulos
+    cubo.position.x = -20;
+    elementos["cubo1"] = cubo;
+
+    let material2 = new THREE.MeshBasicMaterial({color: green});
+    let cubo2 = new THREE.Mesh(geometria, material2);
+    cubo2.position.x = 60;
+    cubo2.position.y = 20;
+    elementos["cubo2"] = cubo2;
+
+    scene.add(cubo);
+    scene.add(cubo2);
 };
+
 
 var init = function(){
     console.log("Teste");
@@ -17,12 +41,29 @@ var init = function(){
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    camera.position.z = 50; //Profundidade: (+)Aproximando e (-)Afastando
+    camera.position.x = 20;
+    camera.position.y = 10;
+    camera.position.z = 80; //Profundidade: (+)Aproximando e (-)Afastando
 
+    criaCubo();
 
-    renderer.render(scene, camera);
+    animation();
+    
 };
 
+var animation = function (){
+
+    requestAnimationFrame(animation); //Adiciona o método na fila de renderização
+
+    elementos["cubo1"].position.x += 0.5;
+
+	elementos["cubo2"].position.x -= velocidade;
+	if (elementos["cubo2"].position.x < -30){
+        velocidade *= -1;
+    }
+		
+    renderer.render(scene, camera);
+}
+
+
 window.onload = this.init
-
-
